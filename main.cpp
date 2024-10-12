6f8e52d1f1e16177175106050149f866c6c9d498
theguyinthe_box <dkaauwai0821@sdsu.edu>
2024-10-12 11:52:52 -0700

    added getopt block for parsing inpout args# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Your branch is up to date with 'origin/master'.## Changes to be committed:#	modified:   main.cpp#

--- main.cpp
+++ main.cpp
@@ -8,6 +8,26 @@
 
 int main(int argc, char *argv[]) {
 
+  int opt;
+
+  while((opt = getopt(argc, argv, "a")) != -1)
+  {
+    switch(opt)
+    {
+      case 'a':
+          printf("option: %c\n", opt);
+      break;
+      case ‘f’:
+        printf("“filename: %s\n”", optarg);
+      break;
+      case ‘:’:
+        printf("“option needs a value\n”");
+      break;
+      case ‘?’:
+        printf(“unknown option: %c\n”, optopt);
+      break;
+    }
+  }
 
   printf("Hello World!\n");
   return 0;
